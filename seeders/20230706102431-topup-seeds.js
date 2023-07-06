eb93ad04050d9950deb4fb25b51c839cac6e5bdc
dante4rt <rxmxdhxni@gmail.com>
2023-07-06 17:26:38 +0700

    add seeding topup

---
+++ seeders/20230706102431-topup-seeds.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const topup = require('../database/topup.json').map((el) => {
+      el.createdAt = el.updatedAt = new Date();
+      return el;
+    });
+
+    await queryInterface.bulkInsert('Topups', topup, {})
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Topups', null, {});
+  },
+};
